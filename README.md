4e36883157cc03a8bda4132c5b23d647d97399c1
Alexey Trofimov <birdwp@mail.ru>
2023-01-20 20:48:26 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## Starter HTML templates
+
+- Simple template;
+- Template with icons from Font Awesome.
+
+*Private repository*
---
+++ minimalweb-start-template/js/main.js
@@ -0,0 +1,12 @@
+/*
+Main js file
+*/
+
+( function( $ ) {
+	'use strict';
+	$( document ).ready( function() {
+
+		console.log( 'Main js added!' );
+
+	} );
+} )( jQuery );
